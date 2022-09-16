582e0ef1c3c3bbbbc7c27af40ad87705de5d2234
AmayaWaka <rmadegwa63@gmail.com>
2022-09-16 22:24:49 +0300

    Added a new collection

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const url = 'mongodb://localhost:27017';
 
 //Database Name
 
-const dbName = 'myproject';
+const dbName = 'fruitsDB';
 
 //Create a new MongoClient
 const client = new MongoClient(url);
@@ -22,3 +22,31 @@ client.connect(function(err){
   const db = client.db(dbName);
   client.close()
 });
+const insertDocuments = function(db, callback){
+  //get the documents collection
+  const collection = db.collection('fruits');
+  //Insert Some documents
+  collection.insertMany([
+{
+  name: "Apple",
+  score: 8,
+  review: "Great fruit"
+},
+{
+  name: "Orange",
+  score: 8,
+  review: "To sweet"
+},
+{
+  name: "Banana",
+  score: 8,
+  review: "Great"
+}
+  ], function(err, result){
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  })
+}
