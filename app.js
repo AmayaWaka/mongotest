0ac3188d00d2f1b95d2cc0bcc4a239a737f9fe35
AmayaWaka <rmadegwa63@gmail.com>
2022-09-18 00:24:30 +0300

    Commented on the mongo server lines

--- app.js
+++ app.js
@@ -4,27 +4,20 @@ const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017");
 
+//Acts as a blueprint for our data
+
 const fruitSchema = new mongoose.Schema({
   name: String,
   rating: Number,
   review: String
 });
+//Creates the collection called Fruits though we are using Fruit
 const Fruit = mongoose.model("Fruit", fruitSchema);
+//Creates the actual data to be inserted
 const fruit = new Fruit({
   name: "Apple",
   rating: 7,
   review: "Pretty Solid as a fruit"
 });
+//Saves the actual data to be inserted into DB
 fruit.save();
-
-
-const findDocuments = function(db, callback){
-  const collection = db.collection('fruits');
-
-  collection.find({}).toArray(function(err, fruits){
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  })
-}
