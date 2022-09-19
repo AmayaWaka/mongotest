8100b1bac9e3ef011c14136f94cc6b8c5391883b
AmayaWaka <rmadegwa63@gmail.com>
2022-09-19 17:47:45 +0300

    Mongo db memory test

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
+// const mongoose = require('mongoose');
+//
+// //Connection URL
+//
+// mongoose.connect("mongodb://localhost:27017/fruitsDB");
+//
+// //Acts as a blueprint for our data
+//
+// const fruitSchema = new mongoose.Schema({
+//   name: String,
+//   rating: Number,
+//   review: String
+// });
+// //Creates the collection called Fruits though we are using Fruit
+// const Fruit = mongoose.model("Fruit", fruitSchema);
+// //Creates the actual data to be inserted
+// const fruit = new Fruit({
+//   name: "Orange",
+//   rating: 7,
+//   review: "Pretty Solid as a fruit"
+// });
+// //Saves the actual data to be inserted into DB
+// fruit.save().then(()=>console.log('Successfully added'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//initialised mongoose constant
 const mongoose = require('mongoose');
+//Created a connection with the db
+mongoose.connect("mongodb://localhost:27017/personDB");
 
-//Connection URL
 
-mongoose.connect("mongodb://localhost:27017");
-
-//Acts as a blueprint for our data
-
-const fruitSchema = new mongoose.Schema({
+//Create Schema for the person db
+const personSchema = new mongoose.Schema({
   name: String,
-  rating: Number,
-  review: String
+  age: Number
+
 });
-//Creates the collection called Fruits though we are using Fruit
-const Fruit = mongoose.model("Fruit", fruitSchema);
-//Creates the actual data to be inserted
-const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
-  review: "Pretty Solid as a fruit"
+//initialized the preson collection
+
+const Person = mongoose.model("person", personSchema );
+//Creted data for entry
+
+const person = new Person({
+  name: "Brad",
+  age: 21
 });
-//Saves the actual data to be inserted into DB
-fruit.save();
+//Inserted collection into db
+person.save();
--- mem.js
+++ mem.js
@@ -1,1 +1,22 @@
-cons mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
+
+const appleSchema = mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+
+const Apple = mongoose.model("Apple", appleSchema);
+
+
+const apple = new Apple({
+  name: "Jane",
+  rating: 5,
+  review: "ugikloijn"
+
+})
+
+apple.save()
